547e6425dad6c1dd959d1cd0bf9f9481aad26eb4
Angelos Kapsimanis <angelos@Angeloss-MacBook-Pro.local>
2020-06-04 22:28:56 +0200

    Working cors

--- web/clear.js
+++ web/clear.js
@@ -1,110 +1,63 @@
-function hello() {
-	console.log("Hello world!");	
-}
-
-
-function renderAttrTable() {
- 	var table = document.getElementById('demo-table');
- 	var devInfo = getDeviceInfo();
-
- 	Object.keys(devInfo).forEach(function(key) {
-		var tr = document.createElement('tr');
-        
-        var td1 = document.createElement('td');
-        td1.appendChild(document.createTextNode(key));
-        tr.appendChild(td1);
-
-        var td2 = document.createElement('td');
-        td2.appendChild(document.createTextNode(devInfo[key]));
-        tr.appendChild(td2);
-        table.appendChild(tr);
-	});
-}
-
-
-function debugDevInfo() {
-	console.log(getDeviceInfo());
-}
-
-
-function getDeviceInfo() {
-	var deviceInfo = {
-		"userAgent": getUserAgent(),
-		"httpHeaders": JSON.stringify(getHTTPHeaders()),
-		"plugins": getPlugins(),
-		"timezoneOffset": getTimezoneOffset(),
-		"timezone": getTimezone(),
-		"screenSize": getScreenSizeColorDepth(),
-		"systemFonts": getSystemFonts(),
-		"cookiesEnabled": areCookiesEnabled(),
-		"language": getLanguage(),
-		"platform": getPlatform(),
-		"oscpu": getOSCPUInfo(),
-	};
-
-	return deviceInfo
-}
-
 
 function getUserAgent() {
-	return navigator.userAgent;
+    return navigator.userAgent;
 }
 
 
 function getHTTPHeaders() {
-	var req = new XMLHttpRequest();
-	req.open('GET', document.location, false);
-	req.send(null);
+    var req = new XMLHttpRequest();
+    req.open('GET', document.location);
+    req.send();
 
-	var data = new Object();
-	var headers = req.getAllResponseHeaders().toLowerCase();
-	var aHeaders = headers.split('\n');
-	var i =0;
+    var data = new Object();
+    var headers = req.getAllResponseHeaders().toLowerCase();
+    var aHeaders = headers.split('\n');
+    var i =0;
 
-	for (i= 0; i < aHeaders.length; i++) {
-	    var thisItem = aHeaders[i];
-	    var key = thisItem.substring(0, thisItem.indexOf(':'));
-	    var value = thisItem.substring(thisItem.indexOf(':')+1);
-		data[key] = value;
-	}
-	return data;
+    for (i= 0; i < aHeaders.length; i++) {
+        var thisItem = aHeaders[i];
+        var key = thisItem.substring(0, thisItem.indexOf(':'));
+        var value = thisItem.substring(thisItem.indexOf(':')+1);
+        data[key] = value;
+    }
+    return data;
 }
 
 
 function getPlugins() {
-	var numPlugins = navigator.plugins.length;
-	var plugins = [];
+    var numPlugins = navigator.plugins.length;
+    var plugins = [];
 
-	for(var i = 0; i < numPlugins; i++)
-	{
-  		plugins.push(navigator.plugins[i].name); 
-	}
-	return plugins;
+    for(var i = 0; i < numPlugins; i++)
+    {
+        plugins.push(navigator.plugins[i].name); 
+    }
+    return plugins;
 }
 
 
 function getTimezoneOffset() {
-	return new Date().getTimezoneOffset();
+    return new Date().getTimezoneOffset();
 }
 
 
 function getTimezone() {
-	return Intl.DateTimeFormat().resolvedOptions().timeZone;
+    return Intl.DateTimeFormat().resolvedOptions().timeZone;
 }
 
 
 function getScreenSizeColorDepth() {
-	return "".concat(
-		window.screen.width.toString(), 
-		"x",
-	 	window.screen.height.toString(),
-	 	"x",
-	 	screen.colorDepth.toString());
+    return "".concat(
+        window.screen.width.toString(), 
+        "x",
+        window.screen.height.toString(),
+        "x",
+        screen.colorDepth.toString());
 }
 
 
 function getSystemFonts() {
-	return window.fonts;
+    return window.fonts;
 }
 
 
@@ -113,27 +66,95 @@ function areCookiesEnabled() {
 
     if (!cookieEnabled){ 
         document.cookie = "testcookie";
-        cookieEnabled = document.cookie.indexOf("testcookie")!=-1;
+        cookieEnabled = document.cookie.indexOf("testcookie")!==-1;
     }
     return cookieEnabled;
 }
 
 
 function getLanguage() {
-	return navigator.language || navigator.userLanguage;
+    return navigator.language || navigator.userLanguage;
 }
 
 
 
 function getPlatform() {
-	return navigator.platform;
+    return navigator.platform;
 }
 
 
 
 function getOSCPUInfo() {
-	return window.navigator.oscpu;
+    return window.navigator.oscpu;
 }
 
 
+function getDeviceInfo() {
+    var deviceInfo = {
+        "userAgent": getUserAgent(),
+        "httpHeaders": JSON.stringify(getHTTPHeaders()),
+        "plugins": getPlugins(),
+        "timezoneOffset": getTimezoneOffset(),
+        "timezone": getTimezone(),
+        "screenSize": getScreenSizeColorDepth(),
+        "systemFonts": getSystemFonts(),
+        "cookiesEnabled": areCookiesEnabled(),
+        "language": getLanguage(),
+        "platform": getPlatform(),
+        "oscpu": getOSCPUInfo(),
+    };
 
+    return deviceInfo
+}
+
+
+function requestInstantScore(apiKey, emailAddress, apiVersion = "0") {
+    var endpointUrl = "https://api.clearhq.ai/instant-score/v" + apiVersion + "/score";
+    var xhr = new XMLHttpRequest();
+
+    xhr.onload = function() {
+        if (xhr.status >= 200 && xhr.status < 300) {
+            var res = JSON.parse(xhr.responseText);
+            console.log(res);
+            return res;
+        } else {
+            return {};
+        }
+    };
+
+    var devInfo = getDeviceInfo();
+
+    var reqBody = {
+        "user_agent": devInfo["userAgent"],
+        "email": emailAddress,
+        "do_not_track": "no"
+    };
+
+    xhr.open("POST", endpointUrl, true);
+    xhr.setRequestHeader("clearhq-key", apiKey);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify(reqBody));
+}
+
+
+function requestEmailScore(apiKey, emailAddress, apiVersion = "0") {
+    var endpointUrl = "http://api.clearhq.ai/email-score/v" + apiVersion + 
+                    "/score?email=" + emailAddress;
+    var xhr = new XMLHttpRequest();
+    // xhr.withCredentials = true;
+
+    xhr.onload = function() {
+        if (xhr.status >= 200 && xhr.status < 300) {
+            var res = JSON.parse(xhr.responseText);
+            console.log(res);
+            return res;
+        } else {
+            return {};
+        }
+    };
+
+    xhr.open("GET", endpointUrl);
+    xhr.setRequestHeader("clearhq-key", apiKey);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.send(JSON.stringify({}));
+}
